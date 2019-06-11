56dd1c08153e45a3e6da8b95b6a39c31a55d5f6c
mutlu <mutlu@localhost>
2019-06-11 16:13:44 +0100

    updated to take oath flow for a token

--- bin/idam-user-token.sh
+++ bin/idam-user-token.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-## Usage: ./idam-user-token.sh [role] [user_id]
-##
-## Options:
-##    - role: Role assigned to user in generated token. Default to `ccd-import`.
-##    - user_id: ID assigned to user in generated token. Default to `1`.
-##
-## Returns a valid IDAM user token for the given role and user_id.
+#!/bin/sh
 
-ROLE="${1:-ccd-import}"
-USER_ID="${2:-1}"
+IMPORTER_USERNAME=$1
+IMPORTER_PASSWORD=$2
+IDAM_URI=$3
+REDIRECT_URI=$4
+CLIENT_ID=$5
+CLIENT_SECRET=$6
 
-curl --silent http://localhost:5000/testing-support/lease -Fid="${USER_ID}" -Frole="${ROLE}"
+code=$(curl ${CURL_OPTS} -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d "" | jq -r .code)
+
+curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | jq -r .access_token
