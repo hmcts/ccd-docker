0dae223568b07d14fbf9ebac873ee515c07e2935
mutlu <mutlu@localhost>
2019-06-17 14:16:35 +0100

    removed echo's for convenience of use in other scripts

--- bin/idam-user-token.sh
+++ bin/idam-user-token.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-IMPORTER_USERNAME=$1
-IMPORTER_PASSWORD=$2
+IMPORTER_USERNAME=${1:-mtl@hmcts.net}
+IMPORTER_PASSWORD=${2:-Pa55word11}
 IDAM_URI="http://localhost:5000"
 REDIRECT_URI="http://localhost:3451/oauth2redirect"
 CLIENT_ID="ccd_gateway"
 CLIENT_SECRET="ccd_gateway_secret"
 
 code=$(curl ${CURL_OPTS} -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d "" | jq -r .code)
-echo "\ncode = ${code}\n"
 
-userToken=$(curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | jq -r .access_token)
-
-echo "\n\nUser Token =\n\n${userToken}\n"
+curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | jq -r .access_token
