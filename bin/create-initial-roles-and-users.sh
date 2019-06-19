c6cedcf419cd2fb3878698bf3669e50ef370aa39
mutlu <mutlu@localhost>
2019-06-19 14:00:20 +0100

    moved role creations before user creations

--- bin/create-initial-roles-and-users.sh
+++ bin/create-initial-roles-and-users.sh
@@ -4,24 +4,24 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest1 auto.test.cnp@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-private auto.test.cnp+private@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-senior auto.test.cnp+senior@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-solicitor auto.test.cnp+solc@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest2 auto.test2.cnp@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest2,caseworker-autotest2-private auto.test2.cnp+private@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest2,caseworker-autotest2-senior auto.test2.cnp+senior@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest2,caseworker-autotest2-solicitor auto.test2.cnp+solc@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest2 auto.test12.cnp@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-private,caseworker-autotest2,caseworker-autotest2-private auto.test12.cnp+private@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-solicitor,caseworker-autotest2,caseworker-autotest2-solicitor auto.test12.cnp+solc@gmail.com password
-../bin/idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-senior,caseworker-autotest2,caseworker-autotest2-senior auto.test12.cnp+senior@gmail.com password;
+./ccd-add-role.sh caseworker-autotest1 PUBLIC
+./ccd-add-role.sh caseworker-autotest1-private PRIVATE
+./ccd-add-role.sh caseworker-autotest1-senior RESTRICTED
+./ccd-add-role.sh caseworker-autotest1-solicitor PRIVATE
+./ccd-add-role.sh caseworker-autotest2 PUBLIC
+./ccd-add-role.sh caseworker-autotest2-private PRIVATE
+./ccd-add-role.sh caseworker-autotest2-senior RESTRICTED
+./ccd-add-role.sh caseworker-autotest2-solicitor PRIVATE
 
-../bin/ccd-add-role.sh caseworker-autotest1 PUBLIC
-../bin/ccd-add-role.sh caseworker-autotest1-private PRIVATE
-../bin/ccd-add-role.sh caseworker-autotest1-senior RESTRICTED
-../bin/ccd-add-role.sh caseworker-autotest1-solicitor PRIVATE
-../bin/ccd-add-role.sh caseworker-autotest2 PUBLIC
-../bin/ccd-add-role.sh caseworker-autotest2-private PRIVATE
-../bin/ccd-add-role.sh caseworker-autotest2-senior RESTRICTED
-../bin/ccd-add-role.sh caseworker-autotest2-solicitor PRIVATE
+./idam-create-caseworker.sh caseworker,caseworker-autotest1 auto.test.cnp@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-private auto.test.cnp+private@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-senior auto.test.cnp+senior@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-solicitor auto.test.cnp+solc@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest2 auto.test2.cnp@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest2,caseworker-autotest2-private auto.test2.cnp+private@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest2,caseworker-autotest2-senior auto.test2.cnp+senior@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest2,caseworker-autotest2-solicitor auto.test2.cnp+solc@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest2 auto.test12.cnp@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-private,caseworker-autotest2,caseworker-autotest2-private auto.test12.cnp+private@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-solicitor,caseworker-autotest2,caseworker-autotest2-solicitor auto.test12.cnp+solc@gmail.com password
+./idam-create-caseworker.sh caseworker,caseworker-autotest1,caseworker-autotest1-senior,caseworker-autotest2,caseworker-autotest2-senior auto.test12.cnp+senior@gmail.com password;
