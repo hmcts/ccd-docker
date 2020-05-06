f298afd93b48275ee141daa339a2e5b1c39db69e
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-05-06 16:00:33 +0100

    RDM-8298 - user creation happy path for single user

--- bulk-user-setup/bulk-user-creation.sh
+++ bulk-user-setup/bulk-user-creation.sh
@@ -1,44 +1,50 @@
 #!/usr/bin/env bash
 
 CSV_FILE_PATH="$1"
-IDAM_ADMIN_USER="$2"
-IDAM_ADMIN_USER_PWD="$3"
+ADMIN_USER="$2"
+ADMIN_USER_PWD="$3"
 IDAM_CLIENT_SECRET="$4"
 ENV=${5:-prod}
 
+REDIRECT_URI="https://create-bulk-user-test/oauth2redirect"
+CLIENT_ID="create-bulk-users"
+
 function usage() {
-  echo "usage: ./bulk-user-creation.sh <user-csv-file-path> <idam-admin-user> <idam-admin-user-pwd> <idam-client-secret> <optional-env>"
+  echo "usage: ./bulk-user-creation.sh <user-csv-file-path> <ccd-admin-user> <ccd-admin-user-pwd> <idam-client-secret> <optional-env>"
+  echo "example: ./bulk-user-creation.sh /Users/xyz/bulk-users.csv ccd-test-admin@mail.com test-password bulk-users-client-secret aat"
 }
 
-if [ -z "${CSV_FILE_PATH}" ] || [ -z "${IDAM_ADMIN_USER}" ] || [ -z "${IDAM_ADMIN_USER_PWD}" ] || [ -z "${IDAM_CLIENT_SECRET}" ]
+function get_idam_url() {
+
+if [ "$ENV" == "prod" ]
+then
+  URL="https://idam-api.platform.hmcts.net"
+else if [ "$ENV" == "local" ]
+then
+  URL="http://localhost:5000"
+else
+  URL="https://idam-api.${ENV}.platform.hmcts.net"
+fi
+fi
+echo "$URL"
+}
+
+if [ -z "${CSV_FILE_PATH}" ] || [ -z "${ADMIN_USER}" ] || [ -z "${ADMIN_USER_PWD}" ] || [ -z "${IDAM_CLIENT_SECRET}" ]
 then
   usage
   exit 1
 fi
 
-IDAM_URL="http://localhost:5000"
-REDIRECT_URI="http://localhost:3451/oauth2redirect"
-CLIENT_ID="create-bulk-users"
+IDAM_URL=$(get_idam_url)
 
-if [ "$ENV" == "prod" ]
-then
-  IDAM_URL="https://idam-api.platform.hmcts.net"
-else if [ "$ENV" == "local" ]
-then
-  IDAM_URL="http://localhost:5000"
-else
-  IDAM_URL="https://idam-api.${ENV}.platform.hmcts.net"
-fi
-fi
-idam_token=$(
-    curl --silent --fail --show-error -X POST "${IDAM_URL}/o/token" -H "accept: application/json" -H "Content-Type: application/x-www-form-urlencoded" \
-    -d "client_id=${CLIENT_ID}&client_secret=${IDAM_CLIENT_SECRET}&grant_type=password&username=${IDAM_ADMIN_USER}&password=${IDAM_ADMIN_USER_PWD}&redirect_uri=${REDIRECT_URI}&scope=create-user" \
-    | jq -r .access_token
+idam_access_token=$(
+    curl --silent --fail --show-error -X POST "${IDAM_URL}/o/token" -H "accept: application/json" \
+        -H "Content-Type: application/x-www-form-urlencoded" \
+        -d "client_id=${CLIENT_ID}&client_secret=${IDAM_CLIENT_SECRET}&grant_type=password&username=${ADMIN_USER}&password=${ADMIN_USER_PWD}&redirect_uri=${REDIRECT_URI}&scope=openid roles create-user" \
+        | jq -r .access_token
      )
 
-echo "$idam_token"
+curl -v -X POST "${IDAM_URL}/user/registration" -H "accept: application/json" -H "Content-Type: application/json" \
+    -H "authorization:Bearer ${idam_access_token}" \
+    -d '{"email":"Joan45.williams@justice.gov.uk","firstName":"Joanna", "lastName": "Williams", "roles":[ "ccd-import"] }'
 
-
-curl -X POST "${IDAM_URL}/user/registration" -H "accept: application/json" -H "Content-Type: application/json" \
-    -H "authorization:Bearer ${idam_token}" \
-    -d '{"email":"Joan.williams@justice.gov.uk","firstName":"Joanna", "lastName": "Williams", "roles":[ "caseworker-autotest2"] }'
