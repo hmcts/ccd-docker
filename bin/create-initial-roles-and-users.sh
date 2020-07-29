6bed932a08278522d8a94c0305020125df29b4d6
mutlu <mutlu.sancaktutar@hmcts.net>
2020-07-29 18:34:58 +0100

    add user creation payloads

--- bin/create-initial-roles-and-users.sh
+++ bin/create-initial-roles-and-users.sh
@@ -67,3 +67,4 @@ cd "$parent_path"
 ./idam-create-caseworker.sh caseworker,caseworker-caa,caseworker-befta_master,caseworker-befta_jurisdiction_1,caseworker-befta_jurisdiction_2 befta.caseworker.caa@gmail.com Pa55word11
 ./idam-create-caseworker.sh caseworker,caseworker-befta_master,caseworker-befta_master-solicitor befta_master.solicitor.becky@gmail.com Pa55word11 Solicington Becky
 ./idam-create-caseworker.sh caseworker,caseworker-befta_master,caseworker-befta_master-solicitor befta_master.solicitor.benjamin@gmail.com Pa55word11 Solicington Benjamin
+./idam-create-caseworker.sh caseworker,caseworker-caa befta.caseworker.caa@gmail.com Pa55word11 CAA Caseworker
