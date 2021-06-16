cad6eaf98637b7e9c7b5f3a4ff32a4ac62780796
Dan Lysiak <daniel.lysiak1@hmcts.net>
2021-06-16 15:16:24 +0100

    RDM-12565 - Introduce RAS

--- bin/add-idam-clients.sh
+++ bin/add-idam-clients.sh
@@ -9,3 +9,5 @@ ${dir}/utils/idam-create-service.sh "ccd_gateway" "ccd_gateway" "ccd_gateway_sec
 ${dir}/utils/idam-create-service.sh "xui_webapp" "xui_webapp" "xui_webapp_secrect" "http://localhost:3455/oauth2/callback" "false" "profile openid roles manage-user create-user"
 
 ${dir}/utils/idam-create-service.sh "ccd_admin" "ccd_admin" "ccd_admin_secret" "https://localhost:3100/oauth2redirect" "false" "profile openid roles"
+
+${dir}/utils/idam-create-service.sh "am_role_assignment" "am_role_assignment" "am_role_assignment_secret" "http://localhost:4096/oauth2redirect" "false" "profile openid roles search-user"
