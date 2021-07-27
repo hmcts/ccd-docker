2fffd24ece582869c027a70bc843a2aa2b77b885
Ruban <ruban72@hotmail.com>
2021-07-27 14:52:53 +0100

    DTSSE-334: Add Xui frontend

--- bin/utils/idam-authenticate.sh
+++ bin/utils/idam-authenticate.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-USERNAME=$1
-PASSWORD=$2
+USERNAME=${1:-fr_applicant_sol@sharklasers.com}
+PASSWORD=${2:-Testing1234}
+IDAM_API_BASE_URL=${IDAM_STUB_LOCALHOST:-https://idam-api.aat.platform.hmcts.net}
 
-curl --silent --show-error --header 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: application/json' -d "username=${USERNAME}&password=${PASSWORD}" "${IDAM_API_BASE_URL:-http://localhost:5000}/loginUser" | docker run --rm --interactive stedolan/jq -r .api_auth_token
+curl --show-error --header 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: application/json' -d "username=${USERNAME}&password=${PASSWORD}" "${IDAM_API_BASE_URL:-http://localhost:5000}/loginUser" | docker run --rm --interactive stedolan/jq -r .api_auth_token
--- bin/utils/idam-user-token.sh
+++ bin/utils/idam-user-token.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-IMPORTER_USERNAME=${1:-ccd.docker.default@hmcts.net}
-IMPORTER_PASSWORD=${2:-Pa55word11}
-IDAM_URI=${IDAM_STUB_LOCALHOST:-http://localhost:5000}
-REDIRECT_URI="http://localhost:3451/oauth2redirect"
-CLIENT_ID="ccd_gateway"
-CLIENT_SECRET="ccd_gateway_secret"
+IMPORTER_USERNAME=${1:-fr_applicant_sol@sharklasers.com}
+IMPORTER_PASSWORD=${2:-Testing1234}
+IDAM_URI=${IDAM_STUB_LOCALHOST:-https://idam-api.aat.platform.hmcts.net}
+REDIRECT_URI="https://div-pfe-aat.service.core-compute-aat.internal/authenticated"
+CLIENT_ID="divorce"
+CLIENT_SECRET="thUphEveC2Ekuqedaneh4jEcRuba4t2t"
 CURL_OPTS="$CURL_OPTS -S --silent"
 code=$(curl ${CURL_OPTS} -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" -d "" | jq -r .code)
 
 curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" -d "" | jq -r .access_token
+
