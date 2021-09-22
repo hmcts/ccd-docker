fc4448740876a4057c66088705d7c4f6c2660ee4
Sita <sita.kandala@hmcts.net>
2021-09-22 11:06:34 +0100

    CCD-276 Mismatch in IDAM client setup of XUI in CCD-Docker and ACA-docker

--- bin/add-idam-clients.sh
+++ bin/add-idam-clients.sh
@@ -6,7 +6,7 @@ dir=$(dirname ${0})
 
 ${dir}/utils/idam-create-service.sh "ccd_gateway" "ccd_gateway" "ccd_gateway_secret" "http://localhost:3451/oauth2redirect" "false" "profile openid roles"
 
-${dir}/utils/idam-create-service.sh "xui_webapp" "xui_webapp" "xui_webapp_secrect" "http://localhost:3455/oauth2/callback" "false" "profile openid roles manage-user create-user"
+${dir}/utils/idam-create-service.sh "xuiwebapp" "xuiwebapp" "OOOOOOOOOOOOOOOO" "http://localhost:3455/oauth2/callback" "false" "profile openid roles manage-user create-user"
 
 ${dir}/utils/idam-create-service.sh "ccd_admin" "ccd_admin" "ccd_admin_secret" "https://localhost:3100/oauth2redirect" "false" "profile openid roles"
 
