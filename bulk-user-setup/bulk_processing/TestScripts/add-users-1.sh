298ae6e922a4bf3c5512b7c71d4db7fdd72eafa9
rajaram <rajaram.nagarajan@solirius.com>
2022-10-24 11:07:25 +0100

    CCD-3575: Refactored files and folders

--- bulk-user-setup/bulk_processing/TestScripts/add-users-1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-dir=$(dirname ${0})
-
-jq -r '.[] | .email + " " + .roles + " " +  .lastName + " " +  .firstName' ${dir}/users-1.json | while read args; do
-  ${dir}/TestScripts/idam-create-caseworker.sh $args
-done
---
+++ bulk-user-setup/test/utils/add-users.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -eu
+
+dir=$(dirname ${0})
+
+jq -r '.[] | .email + " " + .roles + " " +  .lastName + " " +  .firstName' ${dir}/users.json | while read args; do
+  ${dir}/TestScripts/idam-create-caseworker.sh $args
+done
